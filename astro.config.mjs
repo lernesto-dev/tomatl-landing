ce98486db327e16738293a776bd468bd3019384b
Ernesto Rivera <dev@lernesto.net>
2025-03-05 23:10:04 -0600

    Inicio de estilos y header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    icon()
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  image: {
+    service: passthroughImageService()
+  }
+});
