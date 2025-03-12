732cfd070a5c81f4a3e721677678f2c5d881de73
Ernesto Rivera <dev@lernesto.net>
2025-03-11 22:43:48 -0600

    deploy a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     icon()
   ],
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   image: {
     service: passthroughImageService()
-  }
+  },
+
+  adapter: vercel()
 });
